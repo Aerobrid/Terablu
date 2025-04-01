b0f8381031ba935b7cd8a3798b30fd4d1e6eca05
Raj Patel <raj721patel@gmail.com>
2025-03-31 21:25:09 -0500

    Compiling expressions CH 17

--- common.h
+++ common.h
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define DEBUG_PRINT_CODE
 #define DEBUG_TRACE_EXECUTION
 
 // #pragma once (might be better to use instead of #endif but might not work with all compilers)
